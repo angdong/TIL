178a9a56071082ec39bd8ba65d395ce04567bb9c
angdong <yod1073@gmail.com>
2024-01-30 17:08:57 +0900

    Terraform course: RDS

--- terraform-course/12_RDS/vars.tf
+++ terraform-course/12_RDS/vars.tf
@@ -1,5 +1,5 @@
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "ap-northeast-2"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
@@ -16,9 +16,12 @@ variable "AMIS" {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-844e0bf7"
+    ap-northeast-2 = "ami-09eb4311cbaecf89d" # Ubuntu 20.04
   }
+
 }
 
 variable "RDS_PASSWORD" {
+  default = "mypasswordd"
 }
 
